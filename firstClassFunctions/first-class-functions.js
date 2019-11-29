feedddde102c0a89b5218643aa1d97f7a6438031
Peiran Quan <quanpeiran@gmail.com>
2019-11-29 17:42:04 +1300

    closure

--- firstClassFunctions/first-class-functions.js
+++ firstClassFunctions/first-class-functions.js
@@ -4,7 +4,7 @@ function square(x){
 
 function my_map(func, arg_list){
   result = [];
-  for(var i = 0; i < arg_list.length; i++){
+  for(var i = 1; i <= arg_list.length; i++){
     result.push(func(i));
     }
     return result;
@@ -16,3 +16,26 @@ console.log(squares);
 function cube(x) {
   return x*x*x;
 }
+
+function logger(msg){
+	function log_message(){
+    	console.log("Log: " + msg);
+    }
+    return log_message;
+}
+
+log_hi = logger("Hi!")();
+
+function html_tag(tag){
+	function wrap_text(msg){
+    	console.log('<' + tag + '>' + msg + '</' + tag + '>')
+    }
+    return wrap_text
+}
+
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+print_h1('Another Headline!')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
