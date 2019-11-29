2dcd5ef4c80ff106d922443821eb6b0961c0d21a
Peiran Quan <quanpeiran@gmail.com>
2019-11-29 17:37:39 +1300

    Update first-class-function.py
    first class functions with closure

--- firstClassFunctions/first-class-function.py
+++ firstClassFunctions/first-class-function.py
@@ -1,6 +1,9 @@
 def square(x):
   return x * x
 
+def cube(x):
+  return x*x*x
+
 def my_map(func, arg_list):
   result = []
   for i in arg_list:
@@ -11,5 +14,23 @@ squares = my_map(square, [1,2,3,4,5])
 
 print(squares)
 
-def cube(x):
-  return x*x*x
+
+def logger(msg):
+    def log_message():
+        print('Log:', msg)
+    return log_message
+
+log_hi = logger('Hi!')()
+
+
+def html_tag(tag):
+    def wrap_text(msg):
+        print('<{0}>{1}</{0}>'.format(tag, msg))
+    return wrap_text
+
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+print_h1('Another Headline!')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
