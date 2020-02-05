2b1bbff4036249f7a9dba49d9020da1d307f83cf
Peiran Quan <quanpeiran@gmail.com>
2020-02-05 16:39:26 +1300

    encapsulation methods

--- encapsulation/encapsulation.py
+++ encapsulation/encapsulation.py
@@ -1,5 +1,64 @@
 ########## overview play ground ################
 
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+
+class Person:
+  def __init__(self, fname = "DefaultF", lname = "DefaultL"):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(this):
+    print(this.firstname, this.lastname)
+
+class Student(Person):
+  def __init__(self, fname = "DefaultF2", lname = "DefaultL2", age = "DefaultA"):
+    Person.__init__(self, fname , lname )   
+    self.age = age
+
+  def printage(self, texts):
+    print(self.age, texts)
+    
+
+x = Student("Mike", "Olsen", 19)
+x.printname()
+x.printage("yrs old")
+
+# Python program to  
+# demonstrate private members 
+  
+# Creating a Base class 
+class Base: 
+    def __init__(self): 
+        self.a = "init public in Base"
+        self.__c = "init private in Base"
+  
+# Creating a derived class 
+class Derived(Base): 
+    def __init__(self): 
+          
+        # Calling constructor of 
+        # Base class 
+        Base.__init__(self)  
+        print("Calling private member of base class: ") 
+        #print(self.__a) 
+# Driver code 
+obj1 = Base() 
+print(obj1.a) 
+
+obj2 = Derived()
+# Uncommenting print(obj1.c) will 
+# raise an AttributeError 
+  
+# Uncommenting obj2 = Derived() will 
+# also raise an AtrributeError as 
+# private member of base class  
+# is called inside derived class 
+'''
 class Hello:
     def __init__(self, name):
         self.a = 10
@@ -13,6 +72,12 @@ class Hello:
         #return self.a
         #return self._b
         return self.__c
+        
+    def fun(self):
+        print("Public method")
+    def __fun(self):
+        print("Private method")
+        
 hello = Hello("World")
 hello.a = 25
 print(hello.a) # 25
@@ -25,6 +90,9 @@ print(hello.get_value()) # 30
 
 hello.set_value(100)
 print(hello.get_value()) # 100
+
+hello.fun()
+#hello.__fun() # broken
       
 ########## protected ###############
 
